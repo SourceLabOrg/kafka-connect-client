d7889196c579cfa035443a9d219913253deb64f6
Crim <stephen.powis@gmail.com>
2018-03-06 15:57:17 -0500

    wrap up code style violations, add license headers

--- src/main/java/org/sourcelab/kafka/connect/apiclient/rest/handlers/StringResponseHandler.java
+++ src/main/java/org/sourcelab/kafka/connect/apiclient/rest/handlers/StringResponseHandler.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2017 Stephen Powis https://github.com/Crim/pardot-java-client
+ * Copyright 2018 SourceLab.org https://github.com/SourceLabOrg/kafka-connect-client
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
  * documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
