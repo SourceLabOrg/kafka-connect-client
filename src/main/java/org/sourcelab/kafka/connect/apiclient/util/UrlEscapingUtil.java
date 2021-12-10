0008febb90f5e3a90ac555aa32f944d810046bf6
Crim <stephen.powis@gmail.com>
2021-12-10 17:59:16 +0900

    replace guava dependency with other code packaged with HttpComponents

--- src/main/java/org/sourcelab/kafka/connect/apiclient/util/UrlEscapingUtil.java
+++ src/main/java/org/sourcelab/kafka/connect/apiclient/util/UrlEscapingUtil.java
@@ -1,8 +1,8 @@
 package org.sourcelab.kafka.connect.apiclient.util;
 
-import java.util.Objects;
+import org.apache.http.client.utils.URLEncodedUtils;
 
-import static com.google.common.net.UrlEscapers.urlPathSegmentEscaper;
+import java.util.Objects;
 
 /**
  * Intended to provide a shim between Guava so that dependency can be removed in a future release.
@@ -14,9 +14,15 @@ public class UrlEscapingUtil {
      * @param input the literal string to be escaped.
      * @return the escaped form of string.
      * @throws NullPointerException if input string is null.
+     * @throws IllegalArgumentException if string contains badly formed UTF-16 or cannot be escaped for any other reason
      */
     public static String escapePath(final String input) {
         Objects.requireNonNull(input);
-        return urlPathSegmentEscaper().escape(input);
+        final String result = URLEncodedUtils.formatSegments(input);
+        // Strip prepended slash
+        if (result.length() > 0) {
+            return result.substring(1);
+        }
+        return "";
     }
 }
